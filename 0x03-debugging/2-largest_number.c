02ff7842e9e40592b1acd4116a7a45b430311e9f
Udynathcodes <uwahuduakobong@gmail.com>
2023-07-20 10:17:50 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,11 +12,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >= b && b >= c)
+	if (a >= b && (a >= c || b == 0 || c == 0))
 	{
 		largest = a;
 	}
-	else if (b >= a && a >= c)
+	else if (b >= a && (b >= c || a == 0 || c == 0))
 	{
 		largest = b;
 	}
--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -11,13 +11,12 @@ int main(void)
         int a, b, c;
         int largest;
 
-        a = 972;
-        b = -98;
-        c = 0;
+        a = 3;
+        b = 5;
+        c = 2;
 
         largest = largest_number(a, b, c);
 
         printf("%d is the largest number\n", largest);
 
-        return (0);
-}
+        return (0);}
