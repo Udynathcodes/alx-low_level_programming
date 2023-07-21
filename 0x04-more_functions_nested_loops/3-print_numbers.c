7d89a08a23c7626847c203ae71b61bf2fb378f0c
Udynathcodes <uwahuduakobong@gmail.com>
2023-07-21 10:41:52 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_number - print numbers from zero 0 to 9
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+{
+	char r;
+
+	for (r = '0'; r <= '9'; r++)
+	{
+		_putchar('r');
+	}
+	_putchar('\n');
+}
