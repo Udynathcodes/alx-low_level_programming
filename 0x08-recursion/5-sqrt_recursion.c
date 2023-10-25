907d171cb2300d64ffb8955357612fa5502457a4
root <you@example.com>
2023-10-25 06:26:01 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,32 +1,46 @@
 #include "main.h"
-
 /**
- * actual_sqrt_recursion - Help function to compute the square root recursively
- * @n: The non-negative integer for which to find the square root.
- * @i: The current guess for the square root.
- *
- * Return: The square root of 'n', or -1 if 'n' is not a perfect square.
- */
-
-int actual_sqrt_recursion(int n, int i)
-{
-	if (i * i == n)
-	return (i);
-	if (i * i > n)
-	return (-1); / 'n' is not a perfect square.
-	return (actual_sqrt_recursion(n, i + 1));
-}
-
-/**
- * _sqrt_recursion - Returns the natural square root of a number.
- * @n: The number to calculate the square root of.
- *
- * Return: The resulting square root.
- */
-
+  * _sqrt_recursion - print square root
+  * @n: integer
+  * Return: 0
+  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-	return (-1);
-	return (actual_sqrt_recursion(n, 0));
+	if (n == 1 || n == 0)
+	{
+		return (n);
+	}
+	return (_sqr_helper(n, 1, n));
+}
+/**
+  * _sqr_helper - looks for the squareroot
+  * @n: integer
+  * @start: start from 1
+  * @end: end
+  * Return: 0
+  */
+int _sqr_helper(int n, int start, int end)
+{
+	int middle, squared;
+
+	if (start > end)
+	{
+		return (-1);
+	}
+
+	middle = (start + end) / 2;
+	squared = middle * middle;
+
+	if (squared == n)
+	{
+		return (middle);
+	}
+	else if (squared > n)
+	{
+		return (_sqr_helper(n, start, middle - 1));
+	}
+	else
+	{
+		return (_sqr_helper(n, start + 1, end));
+	}
 }
