d17f679b4d29879a64298719697949b253df75b3
root <you@example.com>
2023-11-27 07:00:32 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,47 +1,55 @@
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * create_file - Creates a file and writes text_content to it.
- * @filename: The name of the file to create or truncate.
- * @text_content: The content to write to the file.
+ * custom_strlen - Computes the length of a string manually.
+ * @str: Pointer to the string to be measured.
+ * Return: Length of the string.
+ */
+int custom_strlen(char *str)
+{
+	int len = 0;
+
+	while (*str)
+	{
+		len++;
+		str++;
+	}
+
+	return (len);
+}
+
+/**
+ * create_file - Creates a new file or truncates an existing one.
+ * @file_name: Name of the file to be created or modified.
+ * @content_text: NULL-terminated string to be written to the file.
  * Return: 1 on success, -1 on failure.
  */
-int create_file(const char *filename, char *text_content)
+int create_file(const char *file_name, char *content_text)
 {
+	int fd;
+	ssize_t bytes_written;
 
-
-	int fd, len, bytes_written;
-
-
-	if (filename == NULL)
-
-	return (-1);
-
-
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (fd == -1)
-
-	return (-1);
-
-	if (text_content != NULL)
+	if (file_name == NULL)
 	{
-
-	for (len = 0; text_content[len] != '\0'; len++);
-
-
-	bytes_written = write(fd, text_content, len);
-
-	if (bytes_written == -1 || bytes_written != len)
-	{
-
-	close(fd);
-
-	return (-1);
-
+		return (-1);
 	}
+
+	fd = open(file_name, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (content_text != NULL)
+	{
+		bytes_written = write(fd, content_text, custom_strlen(content_text));
+
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 
 	close(fd);
