c98d9b955f128ac0bfe0d5b111c6dc85286952cf
Udynathcodes <uwahuduakobong@gmail.com>
2024-05-09 12:44:29 +0100

    search_algorithms

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,7 +11,5 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-int interpolation_search(int *array, size_t size, int value);
-listint_t *jump_list(listint_t *list, size_t size, int value);
 
 #endif
